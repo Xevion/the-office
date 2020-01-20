aafd477d554f8ee8ded323656a2d015e5e39e4f9
Xevion <xevioni@yandex.com>
2020-01-20 16:12:30 -0600

    start adding HTML viewing options

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import send_from_directory, redirect, url_for, render_template, request
+from app.models import Season, Episode
 from app import app
-from flask import send_from_directory, redirect, url_for, render_template
 
 @app.route('/')
 def index():
     return 'WIP'
+
+@app.route('/view')
+def view():
+    seasonID = request.args.get('season')
+    episodeNUM = request.args.get('episode')
+
+    season = Season.query.get(int(seasonID))
+    episode = Episode.query.filter_by(season_id=season.id, number=int(episodeNUM))
+
+    if season:
+        if episode:
+            return render_template('episode.html')
+        else:
+            return render_template('season.html', season=Season.query.get())
+    return redirect(url_for('index'))
