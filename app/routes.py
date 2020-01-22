65042c56e196dcd986b9090e53c7b1ebda0d4a14
Xevion <xevioni@yandex.com>
2020-01-21 22:54:16 -0600

    check from query based parameters to path based parameters

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,12 @@ from app import app
 
 @app.route('/')
 def index():
-    return 'WIP'
-
-@app.route('/view')
-def view():
-    season = request.args.get('season', default=-1, type=int)
-    episode = request.args.get('episode', default=-1, type=int)
-
-    if season != -1:
-        if episode != -1:
-            return render_template('episode.html', episode=Episode.query.filter_by(season_id=season, number=episode).first_or_404())
-        else:
-            return render_template('season.html', season=Season.query.filter_by(id=season).first_or_404())
     return render_template('view.html', seasons=Season.query.all())
+
+@app.route('/season/<season>/')
+def season(season):
+    return render_template('season.html', season=Season.query.filter_by(id=season).first_or_404())
+    
+@app.route('/season/<season>/<episode>')
+def episode(season, episode):
+    return render_template('episode.html', episode=Episode.query.filter_by(season_id=season, number=episode).first_or_404())
