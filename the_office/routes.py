08f012a40fbec8bb45c37f4c5db0f2632dee592d
Xevion <xevioni@yandex.com>
2020-08-04 03:30:04 -0500

    validity checks, better page styling, update data

--- the_office/routes.py
+++ the_office/routes.py
@@ -1,4 +1,6 @@
-from flask import current_app, render_template
+from flask import current_app, render_template, abort
+
+from the_office.helpers import check_validity
 
 
 @current_app.route('/')
@@ -9,9 +11,13 @@ def index():
 
 @current_app.route('/<int:season>')
 def view_season(season):
+    if not check_validity(season, 1):
+        abort(404)
     return render_template('season.html', season=season)
 
 
 @current_app.route('/<int:season>/<int:episode>')
 def view_episode(season, episode):
+    if not check_validity(season, episode):
+        abort(404)
     return render_template('episode.html', season=season, episode=episode)
