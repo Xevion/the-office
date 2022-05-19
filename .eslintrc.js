56e570a3a7344b89cb4b980fa586572bc95a42af
Xevion <ryanchwalters@gmail.com>
2022-05-19 03:26:55 -0500

    Add .eslintrc.js for Vue.js project linting & formatting
    - Also changed .gitignore for .env files (previous rules were for Flask .env.local files, no idea why python-dotenv could not read them properly, but whatever).

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        'vue/no-unused-vars': 'error',
+        'indent': ['warn', 4],
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }],
+        'vue/max-attributes-per-line': ["warn", {
+            "singleline": {"max": 5},
+            "multiline": {"max": 2}
+        }]
+    }
+}
