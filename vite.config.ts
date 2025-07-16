cf7cb09687e982b0cc5f2fb85016e7ad2556d584
Xevion <xevion@xevion.dev>
2025-07-16 11:57:18 -0500

    chore: remove env.d.ts, remove @assets vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './public'),
     },
   },
   build: {
